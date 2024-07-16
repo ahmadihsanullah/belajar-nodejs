2456b51efc08ae947b3fa6980a86876135a29d84
ahmadihsanullah <ahma21327ti@student.nurulfikri.ac.id>
2024-07-16 10:56:08 +0700

    menambah package validator, chalk dan nodemon

--- 8-npm/app.js
+++ 8-npm/app.js
@@ -1,1 +1,12 @@
-console.log("Hello World")
+const validator = require('validator');
+const chalk = require('chalk')
+
+console.log(validator.isEmail("ahmad@gmail.com"))
+console.log(validator.isNumeric('9309103'))
+console.log(validator.isMobilePhone('0782345678','id-ID'))
+
+console.log(chalk.italic.bgBlack("hello world"))
+console.log(chalk.italic.bgBlue.bold("hello world"))
+
+const pesan = chalk`halo nama saya {bgGreen.white ahmad} umur saya {bgBlue.bold 21} tahun`
+console.log(pesan);
